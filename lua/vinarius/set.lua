3d6df254636d051250be118a9cb9e017fbfcc1c7
Mark Kraus <8904922+vinarius@users.noreply.github.com>
2023-08-16 15:23:00 -0400

    Update set.lua

--- lua/vinarius/set.lua
+++ lua/vinarius/set.lua
@@ -33,4 +33,4 @@ vim.opt.colorcolumn = "80"
 -- run the :Prettier command on save
 vim.cmd [[autocmd BufWritePre * :Prettier]]
 vim.cmd [[set cursorline]]
-vim.vmd [[set colorcolumn=]]
+vim.cmd [[set colorcolumn=]]

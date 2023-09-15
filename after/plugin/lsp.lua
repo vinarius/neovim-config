70f9ac0334b451c4056b7dae0d60e9d7bacb1243
Mark Kraus <vindevccm@gmail.com>
2023-09-15 17:14:07 -0400

    updates

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -24,12 +24,10 @@ local cmp_select = {behavior = cmp.SelectBehavior.Select}
 local cmp_mappings = lsp.defaults.cmp_mappings({
   ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
   ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
-  -- ['<C-y>'] = cmp.mapping.confirm({ select = true }),
   ['<Tab>'] = cmp.mapping.confirm({ select = true }),
   ["<C-Space>"] = cmp.mapping.complete(),
 })
 
--- cmp_mappings['<Tab>'] = nil
 cmp_mappings['<S-Tab>'] = nil
 
 lsp.setup_nvim_cmp({
@@ -61,9 +59,9 @@ lsp.on_attach(function(client, bufnr)
   vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end, opts)
 end)
 
+lsp.setup()
 
 vim.diagnostic.config({
     virtual_text = true
 })
 
-lsp.setup()
--- lua/vinarius/packer.lua
+++ lua/vinarius/packer.lua
@@ -30,7 +30,7 @@ return require('packer').startup(function(use)
   --   end
   -- }
 
-  use('github/copilot.vim') -- :Copilot setup
+  use('github/copilot.vim')
   use('nvim-treesitter/nvim-treesitter', { run = ':TSUpdate' })
   use('nvim-treesitter/playground')
   use('theprimeagen/harpoon')
